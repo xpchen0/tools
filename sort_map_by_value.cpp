7f1818ae4f04f097d08c073724f445143855e194
xiaoping chen <xiaopingchen@MacBook-Pro-2.local>
2018-09-24 22:26:23 +0800

    map按照value值排序，排序前和排序后map本身存储顺序不变。

--- sort_map_by_value.cpp
+++ sort_map_by_value.cpp
@@ -27,20 +27,25 @@ int cmp(const PAIR& x, const PAIR& y)
 // }
 /**
  * 对map按照value值排序。
+ * 对str_int_map按照值排序，排序之后的结果保存到map_vec，但str_int_map本身的顺序不变。
 */
 int main()
 {
+
     map<string,int> str_int_map;
-    str_int_map.insert(make_pair("a",1));
-    str_int_map.insert(make_pair("b",4));
-    str_int_map.insert(make_pair("c",23));
+    str_int_map.insert(make_pair("b",1));
     str_int_map.insert(make_pair("d",13));
+    str_int_map.insert(make_pair("a",4));
+    str_int_map.insert(make_pair("c",23));
 
     vector<PAIR> map_vec(str_int_map.begin(), str_int_map.end());
     sort(map_vec.begin(), map_vec.end(), cmp);
     
+    cout << " ------排序结果：------- " <<endl;
     for(auto it=map_vec.begin(); it!=map_vec.end(); it++)
         cout << it->first << " ," << it->second << endl;
-    
+    cout << " ------排序后的map:------- " <<endl;
+    for(auto it=str_int_map.begin(); it!=str_int_map.end();it++)
+        cout << it->first << ", " << it->second << endl; //map本身会根据键值排序，不按照插入顺序存储。
     return 0;
 }
